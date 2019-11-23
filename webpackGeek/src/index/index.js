ddeb00b5d778afe1f57c1977e4e1985b300b1839
zhen <770226915@qq.com>
2019-11-23 13:34:58 +0800

    动态加载

--- webpackGeek/src/index/index.js
+++ webpackGeek/src/index/index.js
@@ -4,12 +4,29 @@ import "./index.less";
 import logo from "./img/img.png";
 
 class Search extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = { Text: null }
+  }
+
+  load(){
+    import('./text').then(Text => {
+      this.setState({
+        Text: Text.default
+      })
+    })
+  }
+
   render() {
+    const {Text} = this.state
     return (
       <div className="index">
+        {
+          Text ? <Text/> : null
+        }
         Hello React --babel
         <p className="web-font">哈哈dsadsa</p>
-        <img src={logo}/>
+        <img src={logo} onClick={this.load.bind(this)}/>
       </div>
     );
   }
