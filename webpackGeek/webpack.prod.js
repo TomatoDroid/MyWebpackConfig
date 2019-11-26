e6945b21287175205a48545b2218acd090d9eb40
zhen <770226915@qq.com>
2019-11-26 10:07:44 +0800

    添加eslint

--- webpackGeek/webpack.prod.js
+++ webpackGeek/webpack.prod.js
@@ -53,7 +53,7 @@ module.exports = {
     rules: [
       {
         test: /\.js$/,
-        use: "babel-loader"
+        use: ["babel-loader", "eslint-loader"]
       },
       {
         test: /\.css$/,
